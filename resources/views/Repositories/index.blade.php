5fbf0bcf4a6d709705cbee5cafdb77487756c980
Robert_Pereira <robert.pereira@boostech.com.br>
2024-12-02 16:29:12 -0300

    Modal para criacao de usuario/repositorio

--- resources/views/Repositories/index.blade.php
+++ resources/views/Repositories/index.blade.php
@@ -1,6 +1,6 @@
 @extends('Body.index')
 
-@section('title', 'Usuários')
+@section('title', 'Repositórios')
 
 @section('links')
     @parent
@@ -15,7 +15,7 @@
         </div>
 
         <div class="col-2">
-            <x-button type="button" class="d-flex align-items-center justify-content-center bg-primary"> 
+            <x-button type="button" class="d-flex align-items-center justify-content-center bg-primary" data-bs-toggle="modal" data-bs-target="#modalRepositories"> 
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="me-2 bi bi-plus-lg" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
                 </svg>
@@ -73,4 +73,8 @@
             </div>
         </div>
     </div>
+
+    <x-modal :id="'modalRepositories'" :title="'Salvar repositório'">
+        <h2>Teste aqui :|</h2>
+    </x-modal>
 @endsection
